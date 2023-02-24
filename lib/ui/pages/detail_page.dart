e6d77d517b18383f14cc14f1027c651da59f7fa8
parhanhambali <parhanhambali17@gmail.com>
2023-02-24 21:50:42 +0700

    add file detail_page for add background_image and title and update destination_card

---
+++ lib/ui/pages/detail_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget backgroundImage() {
+      return Container(
+        width: double.infinity,
+        height: 450,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'assets/image_destination1.png'
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget customShadow() {
+      return Container(
+        width: double.infinity,
+        height: 214,
+        margin: EdgeInsets.only(top: 236),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              kWhiteColor.withOpacity(0),
+              Colors.black.withOpacity(0.95)
+            ]
+          )
+        ),
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        child: Column(
+          children: [
+            // Note : Emblem
+            Container(
+              width: 108,
+              height: 24,
+              margin: EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_emblem.png'
+                  )
+                )
+              ),
+            ),
+            // Note : Title
+            Container(
+              margin: EdgeInsets.only(top: 256),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Lake Ciliwung',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: semiBold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'Tangerang',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: light,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        margin: EdgeInsets.only(right: 2),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/icon_star.png',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '4.5',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Stack(
+        children: [
+          backgroundImage(),
+          customShadow(),
+          content(),
+        ],
+      ),
+    );
+  }
+}
