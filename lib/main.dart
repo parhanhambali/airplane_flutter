ea14ad4af484f70f8952dae65141c0fb31117dd5
parhanhambali <parhanhambali17@gmail.com>
2023-03-01 14:59:57 +0700

    fixed firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,21 @@ import 'package:airplane/cubit/page_cubit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
 import 'ui/pages/get_started_page.dart';
 import 'ui/pages/main_page.dart';
 import 'ui/pages/sign_up_page.dart';
 import 'ui/pages/splash_page.dart';
 import 'ui/pages/bonus_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
-} 
+}
 
 class MyApp extends StatelessWidget {
   const MyApp ({ Key? key }) : super( key: key );
